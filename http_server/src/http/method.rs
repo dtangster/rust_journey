581b61ac9bd4beb218772a7b8e42af49a85344cc
David Tang <dtang@egnyte.com>
2021-07-17 20:25:16 -0700

    Try refactoring modules into separate folders

---
+++ http_server/src/http/method.rs
@@ -0,0 +1,10 @@
+pub enum Method {
+    DELETE,
+    GET,
+    HEAD,
+    OPTIONS,
+    PATCH,
+    POST,
+    PUT,
+    TRACE,
+}
--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,8 +1,7 @@
 mod http;
-mod server;
 
 use http::method::Method;
-use server::Server;
+use http::server::Server;
 
 fn main() {
     let server = Server::new("127.0.0.1:8000".to_string());
